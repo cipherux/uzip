a26719bba56841ec595fad1310692e3cfaad1d19
cipherux <cipherux@gmail.com>
2015-05-19 01:57:57 +0100

    Revert "testing"
    This reverts commit 2634a6f98efdf51e6d5f065a0e0c17a9155160da.

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ $conn = mysql_connect("localhost","root","")
 or die("Could not connect to the server.");
 
 mysql_select_db("uzip",$conn)
-or die("Could not select the database."
+or die("Could not select the database.");
 ?>
